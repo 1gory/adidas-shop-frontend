56e559ed7d57b973e296ad79411bfeaabf77d236
Igor Pershin <igor.pershin.me@gmail.com>
2017-05-27 00:11:44 +0300

    'Show' page for xs screen

--- src/Sidebar/Hamburger/index.jsx
+++ src/Sidebar/Hamburger/index.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import styled from 'styled-components';
 import hamburger from './hamburger.svg';
 
-const Image = styled.img`
+const Button = styled.button`
   width: 40px;
   height: 30px;
   display: none;
+  border: none;
+  outline: none;
   cursor: pointer;
+  background: url(${hamburger});
 
   @media (max-width: 768px) {
     display: block;
   }
 `;
 
-export default ({ onClick }) => <Image onClick={onClick} src={hamburger} alt="" />;
+export default ({ onClick }) => <Button onClick={onClick} alt="" />;
